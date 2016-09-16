b64059128c56c3e859cc26b14a7ea0e51447e7de
qnguy019 <qnguy019@ucr.edu>
2016-09-15 20:35:33 -0700

    NewFiles

---
+++ Assets/Scripts/Minigames/DragonGame/DeathWall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathWall : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Checks if the right player hit the right goal
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Death Wall!!!");
+        }
+        else
+        {
+        }
+    }
+
+    /// <summary>
+    /// Helper function that resets each player's position if a player makes a point.
+    /// </summary>
+    void ResetPosition()
+    {
+        GameManager.Instance.Players[1].Key.transform.position = new Vector3(-30f, 5f, 0f);
+        GameManager.Instance.Players[2].Key.transform.position = new Vector3(-20f, 5f, 0f);
+    }
+}
+}
+
---
+++ Assets/Scripts/Minigames/DragonGame/DragonMiniGame.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DragonMiniGame : Minigame
+{
+    GameObject PlayerOne;
+    GameObject PlayerTwo;
+
+    Stats PlayerOneStats;
+    Stats PlayerTwoStats;
+    //Used to display the timer, if needed.
+    public Text Timer;
+    /*Strength*/
+    Dictionary<string, float> P1Skills = new Dictionary<string, float>();
+    Dictionary<string, float> P2Skills = new Dictionary<string, float>();
+
+    /// <summary>
+    /// Description: Calculates the duration of sprint
+    /// </summary>
+    float sprintDurationCalc(Stats Player)
+    {
+        return 1;
+    }
+
+    /// <summary>
+    /// Description: Calculates the cooldown of chicken charges
+    /// </summary>
+    float chickenChargeRateCalc(Stats Player)
+    {
+        return 1;
+    }
+
+    /// <summary>
+    /// Description: Calculates the bonus jump height
+    /// </summary>
+    float jumpHeightCalc(Stats Player)
+    {
+        return 1;
+    }
+    // Use this for initialization
+    void InitSkills()
+    {
+        //Strength P1
+        P1Skills.Add("sprintbar", 100);
+        P1Skills.Add("sprintChargeRate", 1);
+        P1Skills.Add("sprintDuration", sprintDurationCalc(PlayerOneStats));
+        //Intelligence P1
+        P1Skills.Add("chickenBar", 100);
+        P1Skills.Add("chickenChargeRate", chickenChargeRateCalc(PlayerOneStats));
+        P1Skills.Add("chickenCharges", 3);
+        //Dexterity P1
+        P1Skills.Add("jumpHeight", jumpHeightCalc(PlayerOneStats));
+
+        //Strength P2
+        P2Skills.Add("sprintbar", 100);
+        P2Skills.Add("sprintChargeRate", 1);
+        P2Skills.Add("sprintDuration", sprintDurationCalc(PlayerTwoStats));
+        //Intelligence P2
+        P2Skills.Add("chickenBar", 100);
+        P2Skills.Add("chickenChargeRate", chickenChargeRateCalc(PlayerTwoStats));
+        P2Skills.Add("chickenCharges", 3);
+        //Dexterity P2
+        P2Skills.Add("jumpHeight", jumpHeightCalc(PlayerTwoStats));
+    }
+    void Start()
+    {
+        //Set the skill values of each player
+        InitSkills();
+        //Initialize time
+        TimerOn = true;
+        TimeLeft = 5000;
+        PlayerOne = GameManager.Instance.Players[1].Key;
+        PlayerTwo = GameManager.Instance.Players[2].Key;
+
+        PlayerOneStats = GameManager.Instance.Players[1].Value;
+        PlayerTwoStats = GameManager.Instance.Players[2].Value;
+        //Initialize time
+        TimeLeft = 5;
+        //Set player's positions/controls
+        PlayerOne.transform.position = new Vector3(-30f, 6.5f, 0f);
+        PlayerTwo.transform.position = new Vector3(-20f, 2.7f, 0f);
+
+        //Sets the controls, THIS MUST BE CALLED IN ORDER FOR CONTROLS TO WORK
+        SetControls(PlayerOne);
+        SetControls(PlayerTwo);
+    }
+
+    // Update logic for this minigame
+    void Update()
+    {
+        if (!Finished)
+        {
+            if (CountDown(1) != 0)
+            {
+                Finished = true;
+                GameEnd();
+            }
+            else
+            {
+                Timer.text = "Time: " + (int)TimeLeft;
+
+            }
+        }
+
+ public override void UpTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the up key!");
+    }
+
+    public override void LeftTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the left key!");
+    }
+
+    public override void CenterTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the center key!");
+    }
+
+    public override void RightTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the right key!");
+    }
+
+    public override void UpHeldAction(GameObject player)
+    {
+        player.transform.Translate(0f, 0.5f, 0f);
+    }
+
+    public override void LeftHeldAction(GameObject player)
+    {
+        player.transform.Translate(-0.5f, 0f, 0f);
+    }
+
+    public override void CenterHeldAction(GameObject player)
+    {
+        player.transform.Translate(0f, -0.5f, 0f);
+    }
+
+    public override void RightHeldAction(GameObject player)
+    {
+        player.transform.Translate(0.5f, 0f, 0f);
+    }
+
+    public override void UpRelAction(GameObject player)
+    {
+        Debug.Log("Released the up key!");
+    }
+
+    public override void LeftRelAction(GameObject player)
+    {
+        Debug.Log("Released the left key!");
+    }
+
+    public override void CenterRelAction(GameObject player)
+    {
+        Debug.Log("Released the center key!");
+    }
+
+    public override void RightRelAction(GameObject player)
+    {
+        Debug.Log("Released the right key!");
+    }
+
+    public override void GameEnd()
+    {
+        //Checks if the score of the first player is greater than the other player.
+        if (PlayerOneStats.MiniGameScore > PlayerTwoStats.MiniGameScore)
+        {
+            Debug.Log("Player One wins!");
+        }
+        else if (PlayerOneStats.MiniGameScore < PlayerTwoStats.MiniGameScore)
+        {
+            Debug.Log("Player Two wins!");
+        }
+        else
+        {
+            Debug.Log("It is a tie!");
+        }
+        GameManager.Instance.QueueNewGame(); //Starts a new minigame. May modify to change the state of the game manager instead.
+    }
+}
+}
