79cbbf6cf53653817f108ab1b9f72943d95933dc
rwern002 <rwern002@ucr.edu>
2016-09-25 21:28:39 -0700

    Started Minigame

---
+++ Assets/Scripts/Minigames/Hunting/HuntingMinigame.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HuntingMinigame : Minigame {
+
+	private GameObject Player1;
+	private GameObject Player2;
+	private Stats Player1Stats;
+	private Stats Player2Stats;
+	private GameObject Generator;
+	private ObjectPooler Pooler;
+	// Use this for initialization
+	void Start () 
+	{
+		Player1 = GameManager.Instance.Players [1].Key;
+		Player2 = GameManager.Instance.Players [2].Key;
+		Player1Stats = GameManager.Instance.Players [1].Value;
+		Player2Stats = GameManager.Instance.Players [2].Value;
+		SetControls (Player1);
+		SetControls (Player2);
+		Generator = GameObject.Find ("BulletGenerator");
+		Pooler = Generator.GetComponent <ObjectPooler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public override void UpTapAction(GameObject player)
+	{
+		Transform BulletGenePoint = player.transform.Find ("BulletGenPoint").transform;
+		GameObject NewBullet = Pooler.GetPooledObject ();
+		NewBullet.SetActive (true);
+		NewBullet.transform.position = BulletGenePoint.position;
+	}
+
+	public override void LeftTapAction(GameObject player)
+	{
+		
+	}
+
+	public override void CenterTapAction(GameObject player)
+	{
+		
+	}
+
+	public override void RightTapAction(GameObject player)
+	{
+		
+	}
+
+	public override void UpHeldAction(GameObject player)
+	{
+		
+	}
+
+	public override void LeftHeldAction(GameObject player)
+	{
+		player.transform.Translate(-0.5f, 0f, 0f);
+	}
+
+	public override void CenterHeldAction(GameObject player)
+	{
+		player.transform.Translate(0f, -0.5f, 0f);
+	}
+
+	public override void RightHeldAction(GameObject player)
+	{
+		player.transform.Translate(0.5f, 0f, 0f);
+	}
+
+	public override void UpRelAction(GameObject player)
+	{
+		
+	}
+
+	public override void LeftRelAction(GameObject player)
+	{
+		
+	}
+
+	public override void CenterRelAction(GameObject player)
+	{
+		
+	}
+
+	public override void RightRelAction(GameObject player)
+	{
+		
+	}
+
+	public override void GameEnd ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Minigames/Hunting/Monster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+	private GameObject DestructPoint;
+	public float WalkSpeed;
+	// Use this for initialization
+	void Start () 
+	{
+		DestructPoint = GameObject.Find ("MonsterPass");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (this.gameObject.transform.position.y < DestructPoint.transform.position.y) 
+		{
+			gameObject.SetActive (false);
+		} 
+
+		else 
+		{
+			gameObject.transform.Translate (0, WalkSpeed, 0);
+		}
+
+	}
+}
