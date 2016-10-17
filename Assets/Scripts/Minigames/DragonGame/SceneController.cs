8dc7f1a1abbd3ad7c58281bfaccff508ffa354ee
qnguy019 <qnguy019@ucr.edu>
2016-10-17 06:57:43 -0700

    Redid Project

--- Assets/Scripts/Minigames/DragonGame/SceneController.cs
+++ Assets/Scripts/Minigames/DragonGame/SceneController.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class SceneController : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
