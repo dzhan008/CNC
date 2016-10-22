350e50d756ac98938c70973e905f3363cc4fad0c
Rica F <rfeng001@ucr.edu>
2016-10-22 13:44:55 -0700

    pong ball physics

---
+++ Assets/Scripts/Minigames/Pongout/BallPhysics.cs
@@ -0,0 +1,33 @@
+﻿/*
+Created By: Rica Feng
+Description: Controls ball physics
+Requirements: None
+*/
+
+
+using UnityEngine;
+using System.Collections;
+
+public class BallPhysics : MonoBehaviour
+{
+    [SerializeField]
+    private float BallSpeed = 10f;
+
+    [SerializeField]
+    private Vector2 StartDirection;
+
+    public Rigidbody2D RB;
+
+	// Use this for initialization
+	void Start ()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        RB.AddForce(StartDirection, ForceMode2D.Force);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RB.velocity = BallSpeed * (RB.velocity.normalized);
+    }
+}
---
+++ Assets/Scripts/Minigames/Pongout/PongOutMain.cs
@@ -0,0 +1,146 @@
+﻿/*
+Created By: Rica Feng
+Description: Driving script for Pongout minigame
+Requirements: None
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PongOutMain : Minigame
+{
+    [SerializeField]
+    private float Speed = 30f;
+
+    private GameObject PlayerOne;
+    private GameObject PlayerTwo;
+
+    private Stats PlayerOneStats;
+    private Stats PlayerTwoStats;
+
+    //Used to display the timer, if needed.
+    //public Text Timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Minigame Initializing!");
+        //Initialize time
+        TimerOn = true;
+        TimeLeft = 5;
+
+        PlayerOne = GameManager.Instance.Players[1].Key;
+        PlayerTwo = GameManager.Instance.Players[2].Key;
+
+        PlayerOneStats = GameManager.Instance.Players[1].Value;
+        PlayerTwoStats = GameManager.Instance.Players[2].Value;
+
+        //Set player's positions/controls
+        //PlayerOne.transform.position = new Vector3(-30f, 5f, 0f);
+        //PlayerTwo.transform.position = new Vector3(-20f, 5f, 0f);
+
+        //Sets the controls, THIS MUST BE CALLED IN ORDER FOR CONTROLS TO WORK
+        SetControls(PlayerOne);
+        SetControls(PlayerTwo);
+    }
+
+    // Update logic for this minigame
+    void Update()
+    {
+        if (!Finished && TimerOn)
+        {
+            if (CountDown(1) != 0)
+            {
+                Finished = true;
+                GameEnd();
+            }
+            else
+            {
+                //Timer.text = "Time: " + (int)TimeLeft;
+            }
+        }
+    }
+
+    public override void UpTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the up key!");
+    }
+
+    public override void LeftTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the left key!");
+    }
+
+    public override void CenterTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the center key!");
+    }
+
+    public override void RightTapAction(GameObject player)
+    {
+        Debug.Log("Tapped the right key!");
+    }
+
+    public override void UpHeldAction(GameObject player)
+    {
+
+    }
+
+    public override void LeftHeldAction(GameObject player)
+    {
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        rb.MovePosition(rb.position + new Vector2(-Speed, 0) * Time.deltaTime);
+    }
+
+    public override void CenterHeldAction(GameObject player)
+    {
+
+    }
+
+    public override void RightHeldAction(GameObject player)
+    {
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        rb.MovePosition(rb.position + new Vector2(Speed, 0) * Time.deltaTime);
+    }
+
+    public override void UpRelAction(GameObject player)
+    {
+        Debug.Log("Released the up key!");
+    }
+
+    public override void LeftRelAction(GameObject player)
+    {
+        Debug.Log("Released the left key!");
+    }
+
+    public override void CenterRelAction(GameObject player)
+    {
+        Debug.Log("Released the center key!");
+    }
+
+    public override void RightRelAction(GameObject player)
+    {
+        Debug.Log("Released the right key!");
+    }
+
+    public override void GameEnd()
+    {
+        //Checks if the score of the first player is greater than the other player.
+        if (PlayerOneStats.MiniGameScore > PlayerTwoStats.MiniGameScore)
+        {
+            Debug.Log("Player One wins!");
+        }
+        else if (PlayerOneStats.MiniGameScore < PlayerTwoStats.MiniGameScore)
+        {
+            Debug.Log("Player Two wins!");
+        }
+        else
+        {
+            Debug.Log("It is a tie!");
+        }
+        GameManager.Instance.QueueNewGame(); //Starts a new minigame. May modify to change the state of the game manager instead.
+    }
+
+
+}
