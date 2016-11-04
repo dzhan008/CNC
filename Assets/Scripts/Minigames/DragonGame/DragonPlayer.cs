0af0692aaad24d22c952b63ad26f957d6339e17e
qnguy019 <qnguy019@ucr.edu>
2016-11-03 22:30:55 -0700

    Added Sprint Bar

---
+++ Assets/Scripts/Minigames/DragonGame/DragonPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonPlayer : MonoBehaviour {
+    [SerializeField]
+    private PlayerStat sprint;
+
+    //This needs to be called before our bar script
+    //so much call Awake instead of Start
+	private void Awake()
+    {
+        sprint.Initialize();
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            sprint.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            sprint.CurrentVal += 10;
+        }
+	
+	}
+}
